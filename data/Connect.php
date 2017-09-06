3ab575ba6019a32c64fee663d84e752f6f2002fd
klvst3r <klvst3r@gmail.com>
2017-09-06 18:19:10 -0500

    Create Connection to DB

---
+++ data/Connect.php
@@ -0,0 +1,24 @@
+<?php
+ class Connect {
+ 	/*
+ 	 * Connection to DataBase 
+ 	 * @return conexion PDO
+ 	 *
+ 	 */ 
+
+	public static function connection(){
+		try {
+			//var of Connecton
+			$cn = new PDO("mysql:host=localhost;dbname=phpHomework","dev","desarrollo");
+
+			echo "Connection Successful";
+			return $cn;
+
+		} catch(PDOException $ex) {
+			die($ex->getMessage());
+		  }
+	}
+}
+
+/*Test of Connection*/
+/*Connect::connection();*/
