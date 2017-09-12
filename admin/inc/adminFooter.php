bd79b865e6786720a032b109bd65270d00401d94
klvst3r <klvst3r@gmail.com>
2017-09-12 13:31:55 -0500

    Creation page admin admin/index.php

--- admin/inc/adminFooter.php
+++ admin/inc/adminFooter.php
@@ -1,1 +1,53 @@
-footer del administrador
+ 
+ <!-- jQuery -->
+ <script src="assets/jquery/jquery.min.js"></script>
+
+ <!-- Plugin jQuery Confirm -->
+<script src="assets/js/jquery-confirm.min.js"></script>
+
+ <!-- Bootstrap Core JavaScript -->
+ <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+ <!-- Metis Menu Plugin JavaScript -->
+ <script src="assets/metisMenu/metisMenu.min.js"></script>
+
+ <!-- Custom Theme JavaScript -->
+ <script src="assets/js/sb-admin-2.js"></script>
+
+ <!-- Angular JS -->
+ <script src="assets/js/angular.min.js"></script>
+ <!-- <script src="assets/js/controladores/VentaController.js"></script> -->
+
+<script>
+
+function confirmar(url){
+	$.confirm({
+		title: '¿Deseas eliminar este registro?',
+		content: 'Se eliminará completamente del sistema. \n Si elimino por error, esta acción se cancelara en 5 segundos.', 
+		icon: 'fa fa-question',
+		theme: 'modern', //modern, material, bootstrap, supervan
+		closeIcon: true,
+		animation: 'scale',
+		type: 'red',
+		autoClose: 'cancelAction|5000',
+    	escapeKey: 'cancelAction',
+		buttons: {
+			confirm: {
+				btnClass: 'btn-red',
+				text: 'Confirmar',
+				action: function() {
+					window.location.href=url;
+				}
+			},
+			cancelAction: {
+				btnClass: 'btn-success',
+				text: 'Cancelar',
+				action: function() {
+					//$.alert('Cancelado!');
+				}
+			}
+		}
+
+	});
+}
+ </script>
--- admin/inc/adminMenu.php
+++ admin/inc/adminMenu.php
@@ -1,1 +1,90 @@
-menu de administrador y validación contra accesos sin logearse
+<?php
+
+//if(isset($_SESSION["usuario"])){
+?>       
+       <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">Klvst3r - phpHomeWork</a>
+        </div>
+        <!-- /.navbar-header -->
+
+        <ul class="nav navbar-top-links navbar-right">
+      
+            <!-- /.dropdown -->
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                    <i class="fa fa-user fa-fw"></i> session <i class="fa fa-caret-down"></i>
+                </a>
+                <ul class="dropdown-menu dropdown-user">
+                    <li><a href="#"><i class="fa fa-user fa-fw"></i> Perfil</a>
+                    </li>
+                    <li><a href="#"><i class="fa fa-gear fa-fw"></i> Configuración</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li><a href="salir.php"><i class="fa fa-sign-out fa-fw"></i> Salir</a>
+                    </li>
+                </ul>
+                <!-- /.dropdown-user -->
+            </li>
+            <!-- /.dropdown -->
+        </ul>
+        <!-- /.navbar-top-links -->
+
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse">
+                <ul class="nav" id="side-menu">
+                    <li class="sidebar-search">
+                        <div class="input-group custom-search-form">
+                            <input type="text" class="form-control" placeholder="Buscar Usuario...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                        <!-- /input-group -->
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-dashboard fa-fw"></i> Principal</a>
+                    </li>
+                    <li>
+                       
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-table fa-fw"></i> Productos</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-edit fa-fw"></i> Ventas</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-wrench fa-fw"></i> Administración<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="usuarios.php">Usuarios</a>
+                            </li>
+                            <li>
+                                <a href="privilegios.php">Privilegios</a>
+                            </li>
+                        
+                        </ul>
+                        <!-- /.nav-second-level -->
+                    </li>
+                </ul>
+            </div>
+            <!-- /.sidebar-collapse -->
+        </div>
+        <!-- /.navbar-static-side -->
+    </nav>
+
+<?php
+/*}else{
+            //echo "No existen variables de sesión";
+            return vista::crear("admin.logout");
+        }*/
+?>
