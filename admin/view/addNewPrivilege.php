9d5e36915b909fabdee19a4d8a3eba4f8e8a681e
klvst3r <klvst3r@gmail.com>
2017-09-18 20:05:41 -0500

    Begin and send first data to Class Privileges

--- admin/view/addNewPrivilege.php
+++ admin/view/addNewPrivilege.php
@@ -1,1 +1,29 @@
-addNewPrivilege
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php 
+    include'inc/adminHead.php'; 
+    include 'sql/Privilege.php';
+    ?>
+
+</head>
+<body>
+<div id="wrapper">
+
+	<!-- Navigation -->
+
+<?php
+$query = " SELECT id_priv as ID, desc_priv as Privilegio FROM privileges";
+Privilege::regPrivilege($query);
+
+?>
+
+</div>
+<!-- /#wrapper -->
+
+<?php  include 'inc/adminFooter.php'; ?>
+
+</body>
+
+</html>    
