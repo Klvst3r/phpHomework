e0e078fefe06333194234d047c74482f2c2344f6
klvst3r <klvst3r@gmail.com>
2017-09-27 14:54:19 -0500

    View for delete record

---
+++ admin/view/delPrivilege.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+    include 'inc/adminHead.php'; 
+    include 'sql/PrivilegeSQL.php';
+    //include '../helps/helps.php';
+
+    ?>
+</head>
+<body>
+    <div id="wrapper">
+        <?php  include'inc/adminMenu.php'; ?>
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Panel de Administración
+                    </h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <div class="col-sm-9 col-sm-offset-1">
+            
+
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Registro Eliminado</h3>
+                </div>
+                
+                <div class="panel-body ">
+                    <!-- Formulario Cambio de Password -->
+                   <?php
+
+                   
+
+                   ?>
+
+	           <!-- End Formulario Cambio de Passwordl -->
+                </div>
+            
+            </div>          
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <?php  include 'inc/adminFooter.php'; ?>
+</body>
+
+</html>
