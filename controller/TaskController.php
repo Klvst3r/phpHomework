e4a26dcd75c73b7af676bc16db864a5513ca621c
klvst3r <klvst3r@gmail.com>
2017-10-12 11:23:38 -0500

    Create Task Controller and add method to reg Task

--- controller/TaskController.php
+++ controller/TaskController.php
@@ -2,104 +2,33 @@
 //After generating the entity we create the controller
 //we import TaskDao to use it in the defined class
 include '../data/TaskDAO.php';
-class UserController {
-	//defining the fncion with two parameters, packages the user and password in an object called entity user
-	// and will send it to UserID to validate it
-	public static function login($user_name, $password) {
-		//It is instantiated and not imported because it is already added from the entity
-		$obj_user = new User();
-		//We send the object to the user from the application
-		$obj_user->setUser_name($user_name);
-		$obj_user->setUser_password_hash($password);
-		//The class we send by the method the parameters of the object (user and password) as parameters of the method
-		//returning the result of the operation
-		return UserDAO::login($obj_user);
-	}
-
-	// We get the data from the database
-	public function getUser($user_name, $user_password){
-
-		$obj_user = new User();
-
-		$obj_user->setUser_name($user_name);
-		$obj_user->setUser_password_hash($user_password);
-		//We send the object from UserDAO
-		return UserDAO::getUser($obj_user);
-
-	}
-
-	/*function tableUsers(){
-		echo "Estamos en UserControls";
-	}*/
-
-	//UserController::regPirvilege($user_name, $user_password, $user_email, $name, $privilege, $date_reg);
-	public function regUser($user, $pass, $email, $name, $priv, $date){
+class TaskController {
+	
+	/**
+	 * Create a new Object for Task and send data to the object to process
+	 * @param  [type] $area  		[Receive id_area]
+	 * @param  [type] $user		  	[Receive id_user]
+	 * @param  [type] $desc 		[Receive description of task]
+	 * @param  [type] $date  		[Receive date of task in format YY-mm-dd HH:mm:ss]
+	 */
+	public function regTask($area, $user, $desc, $date){
 		//1. Ceerate Object to Work
-		$obj_user = new User();
+		$obj_task = new Task();
 
-		/*
-		 *
-
-	public function setName($name){
-		$this->name = $name;
-	}
-
-	public function setUser_name($user_name){
-		$this->user_name = $user_name;
-	}
-
-	public function setUser_email($user_email){
-		$this->user_email = $user_email;
-	}
-
-	public function setUser_password_hash($user_password_hash){
-		$this->user_password_hash = md5($user_password_hash);
-	}
-
-	public function setId_Priv($id_priv){
-		$this->id_priv = $id_priv;
-	}
-
-	public function setDate_reg($date_reg){
-		$this->date_reg = $date_reg;
-	}
-		 */
 		//Set data to te model
-		$obj_user->setName($name);
-		$obj_user->setUser_name($user);
-		$obj_user->setUser_email($email);
-		$obj_user->setUser_password_hash($pass);
-		$obj_user->setId_Priv($priv);
-		$obj_user->setDate_reg($date);
+		$obj_task->setId_area($area);
+		$obj_task->setId_user($user);
+		$obj_task->setDesc_task($desc);
+		$obj_task->setDate_task($date);
+
 
 		//We send the object from UserDAO
-		//return UserDAO::regUser($obj_user);
-		/*echo "Username: " . $name . "<br/>";
-		echo "Name: " . $user . "<br/>";
-		echo "Email: " . $email . "<br/>";
-		echo "Password: " . $pass . "<br/>";
-		echo "Privilege: " . $priv . "<br/>";
-		echo "Date: " . $date . "<br/>";*/
-
-		//We send the object from UserDAO
-		return UserDAO::regUser($obj_user);
+		return TaskDAO::regTask($obj_task);
 
 
-	}//Function regUSer
+	}//Function regTask
 
-	public function changePass($id, $lastPass, $pass, $rewPass){
-		//1. Ceerate Object to Work
-		$obj_user = new User();
 
-		$obj_user-> setId_user($id);
-		$obj_user->	setUser_password_hash($lastPass);
-		$obj_user-> setNew_pass($pass);
-		$obj_user->	setRewrite_pass($rewPass);
-
-		//We send the object from UserDAO
-		return UserDAO::changePass($obj_user);
-
-	}//Function
 
 	public function updateUser($id, $user, $pass, $email, $name, $priv){
 		//1. Ceerate Object to Work
