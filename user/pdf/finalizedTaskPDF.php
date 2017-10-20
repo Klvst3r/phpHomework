bd6ba06da55bf8ddc52995770bba73f496a3e566
klvst3r <klvst3r@gmail.com>
2017-10-19 19:12:58 -0500

    Add library and references of FPDF

--- user/pdf/finalizedTaskPDF.php
+++ user/pdf/finalizedTaskPDF.php
@@ -14,15 +14,18 @@ function LoadData($file)
 }
 
 function Header(){
+
+// Logo
+$this->Image('../assets/img/logo/hw.jpg',8,3,25);
+
         $this->SetFont('Arial','B',15);
         $this->setX(20);
 
-// $this->Line(20, 6, 195, 6); // 20mm from each edge
+ $this->Line(20, 34, 195, 34); // 20mm from each edge
+ $this->Line(20, 280, 200, 280); // 20mm from each edge
 
-//$this->Line(20, 260.5, 200, 260.5); // 20mm from each edge
-//$this->Line(20, 261.5, 200, 261.5); // 20mm from each edge
-// $this->Line(20, 262.5, 200, 262.5); // 20mm from each edge
         $this->SetFont('Arial','B',24);
+        $this->SetX(55);
         $this->Cell(200,10,utf8_decode("Sistema de Información"));
         $this->Ln();
         $this->SetX(75);
@@ -36,11 +39,11 @@ function Header(){
         $this->Cell(39,6,"",1);
         $this->setY(19);
         $this->setX(160);
-        $this->Cell(13,6,"Día",1);
+        $this->Cell(13,6,utf8_decode("Día"),1);
         $this->setX(160+13);
-        $this->Cell(13,6,"MES",1);
+        $this->Cell(13,6,"Mes",1);
         $this->setX(160+26);
-        $this->Cell(13,6,"A".utf8_decode("Ñ")."O",1);
+        $this->Cell(13,6,utf8_decode("Año"),1);
         $this->setY(25);
         $this->setX(160);
         $this->Cell(13,6,"",1);
@@ -58,6 +61,10 @@ function ImprovedTable($data)
         $this->SetFont('Arial','B',8);
         $this->setY(33);
         $this->setX(20);
+        
+        //Salto de línea
+        $this->Ln(0);
+
         $this->Cell(0,10,"AV. CESAR SANDINO NO. 313 (FRENTE AL SEGURO SOCIAL) COL. 1RO. DE MAYO VILLAHERMOSA, TABASCO. TEL: 352 29 10");
         $this->setY(40);
         $this->setX(20);
@@ -395,12 +402,208 @@ function ImprovedTable($data)
 }
 
 // Tabla coloreada
+
+// Pie de página
+function Footer()
+{
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
 }
 
+
+// Tabla coloreada
+function FancyTable($header, $data)
+{
+
+      $this->setY(31);
+      $this->setX(20);
+      $this->SetFont('Arial','B',8);
+      $this->setY(43);
+      $this->setX(30);
+    // Colores, ancho de línea y fuente en negrita
+    $this->SetFillColor(128,0,0);
+    $this->SetTextColor(255);
+    $this->SetDrawColor(128,0,0);
+    $this->SetLineWidth(.3);
+    $this->SetFont('','B');
+    // Cabecera
+    $w = array(40, 35, 45, 40);
+    for($i=0;$i<count($header);$i++)
+        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
+    $this->Ln();
+    // Restauración de colores y fuentes
+    $this->SetFillColor(224,235,255);
+    $this->SetTextColor(0);
+    $this->SetFont('');
+    // Datos
+    $fill = false;
+
+    if(is_array($data)){
+
+    foreach($data as $row)
+    {
+        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+        $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R',$fill);
+        $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R',$fill);
+        $this->Ln();
+        $fill = !$fill;
+    }
+   }
+
+    // Línea de cierre
+    $this->Cell(array_sum($w),0,'','T');
+}//Fancy Table
+
+
+function TituloArchivo($num,$label)
+{
+   $this->SetY(40);
+//Arial 12
+$this->SetFont('Arial','',12);
+//Color de fondo
+$this->SetFillColor(230,230,230); //fondo gris
+//Título
+$this->Cell(0,6,"Archivo $num : $label",0,1,'L',true);
+//Salto de línea
+$this->Ln(4);
+}
+
+function CuerpoArchivo($file)
+{
+//Leemos el fichero
+$f=fopen($file,'r');
+$txt=fread($f,filesize($file));
+fclose($f);
+//Times 12
+$this->SetFont('Times','',12);
+//Imprimimos el texto justificado
+$this->MultiCell(0,5,$txt);
+//Salto de línea
+$this->Ln();
+
+}
+
+
+function ImprimirArchivo($num,$title,$file)
+{
+$this->AddPage();
+$this->TituloArchivo($num,$title);
+//$this->CuerpoArchivo("cuerpo del archivo");
+//$this->CuerpoArchivo($file);
+}
+
+//Tabla Basica
+function TablaBasica($header)
+   {
+    $this->setX(20);
+    //Cabecera
+    foreach($header as $col)
+    $this->Cell(40,7,$col,1);
+
+    $this->Ln();
+    $this->setX(20);
+   
+      $this->Cell(40,5,"hola",1);
+      $this->Cell(40,5,"hola2",1);
+      $this->Cell(40,5,"hola3",1);
+      $this->Cell(40,5,"hola4",1);
+      $this->Ln();
+      $this->setX(20);
+      $this->Cell(40,5,"linea ",1);
+      $this->Cell(40,5,"linea 2",1);
+      $this->Cell(40,5,"linea 3",1);
+      $this->Cell(40,5,"linea 4",1);
+   }
+
+
+
+   function TablaColores($header)
+{
+   $this->SetX(30);
+   $this->SetY(90);
+//Colores, ancho de línea y fuente en negrita
+$this->SetFillColor(230,230,230);
+$this->SetTextColor(2);
+$this->SetDrawColor(128,0,0);
+$this->SetLineWidth(.3);
+$this->SetFont('','B');
+//Cabecera
+
+for($i=0;$i<count($header);$i++)
+$this->Cell(40,7,$header[$i],1,0,'C',1);
+$this->Ln();
+//Restauración de colores y fuentes
+$this->SetFillColor(224,235,255);
+$this->SetTextColor(0);
+$this->SetFont('');
+//Datos
+$fill=false;
+
+$this->Cell(40,6,"hola",'LR',0,'L',$fill);
+$this->Cell(40,6,"hola2",'LR',0,'L',$fill);
+$this->Cell(40,6,"hola3",'LR',0,'R',$fill);
+$this->Cell(40,6,"hola4",'LR',0,'R',$fill);
+$this->Ln();
+$fill=true;
+$this->Cell(40,6,"col",'LR',0,'L',$fill);
+$this->Cell(40,6,"col2",'LR',0,'L',$fill);
+$this->Cell(40,6,"col3",'LR',0,'R',$fill);
+$this->Cell(40,6,"col4",'LR',0,'R',$fill);
+$fill=!$fill;
+$this->Ln();
+$this->Cell(160,0,'','T');
+}  
+
+
+
+
+
+} //Class
+
+//$pdf = new PDF();
+//$pdf->AddPage();
+//$pdf->Header();
+
+
+// Creación del objeto de la clase heredada
 $pdf = new PDF();
+$pdf->AliasNbPages();
+
+$header = array(utf8_decode('País'), 'Capital', 'Superficie (km2)', 'Pobl. (en miles)');
+// Carga de datos
+$data = $pdf->LoadData('pdf/paises.txt');
+
+
+
+
+$pdf->SetFont('Times','',12);
+
 $pdf->AddPage();
-// $pdf->Header();
 $pdf->ImprovedTable("hola");
+$pdf->AddPage();
+$pdf->FancyTable($header, $data);
+
+//$pdf->ImprimirArchivo(1,'Archivo de prueba ','prueba1.txt');
+$pdf->ImprimirArchivo(1,'Titulo de prueba ','texto');
+
+//Tabla Basica
+//Títulos de las columnas
+$header=array('Columna 1','Columna 2','Columna 3','Columna 4');
+$pdf->SetY(65);
+
+$pdf->TablaBasica($header); 
+
+
+$pdf->SetY(65);
+
+$pdf->TablaColores($header);
+
+
 
 //echo $name;
 $pdf->Output();
